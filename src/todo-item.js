392b8b0fe483d02e18af8531161df98f47505f02
krokhale <krishna.rokhale@gmail.com>
2021-03-24 19:10:11 -0700

    checking in

--- src/todo-item.js
+++ src/todo-item.js
@@ -1,11 +1,40 @@
 import React, {useState, useEffect} from 'react';
 
 function TodoItem(props) {
+
+    const markAsCompleted = async () => {
+        console.log('mark this item as completed')
+        // make a PUT request and update that particular item and set completed to true
+
+        let res = await fetch(`http://localhost:3000/api/v1/todos/${props.item.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({completed: true})
+        });
+
+        let data = await res.json();
+        console.log(data);
+        props.fetchItems();
+
+    };
+
+    const deleteItem = async () => {
+        let res = await fetch(`http://localhost:3000/api/v1/todos/${props.item.id}`, {
+            method: 'DELETE'
+        });
+        props.fetchItems();
+    };
+
+
     return (
         <li className={' border-b-2 border-cool-gray-200 p-1'}>
-            <input type="checkbox"/>&nbsp;&nbsp;
-            <span className={'text-gray-700 font-semibold text-sm'}>{props.item.str}</span>
-            <i className={'fa fa-trash text-red-400 float-right'} />
+            {!props.item.completed &&  <span><input onClick={markAsCompleted} type="checkbox"/>&nbsp;&nbsp;</span>}
+            {/*<span className={'text-gray-700 font-semibold text-sm'}>{props.item.str}</span>*/}
+            <span className={props.item.completed ? 'line-through text-gray-700 font-semibold text-sm' : 'text-gray-700 font-semibold text-sm'}>{props.item.itemTxt}</span>
+            {!props.item.completed && <i className={'fa fa-trash text-red-400 float-right cursor-pointer'} onClick={deleteItem} />}
         </li>
     );
 }
--- src/todos.js
+++ src/todos.js
@@ -6,7 +6,37 @@ function Todos(props) {
     const [inputText, setInputText] = useState('');
     const [list, setList] = useState([]);
 
-    const [currentTab, setCurrentTab] = useState('all');
+    const [currentTab, setCurrentTab] = useState('active');
+
+
+
+
+    // GET /todos
+    // GET /todos/1
+    // POST /todos
+    // PUT /todos
+    // DELETE /todos
+    const fetchItems = async () => {
+        console.log('this is going to fetch the users from the back end');
+        let u = await fetch('http://localhost:3000/api/v1/todos');
+        let data = await u.json();
+        console.log('todos list from the backend API', data);
+        setList(data);
+        // setUsers(data);
+        // 1. Perform a fetch call
+        // 2. Once the fetch call is successful, update the state variable for the users
+        // 3. Display the users
+    };
+
+
+    useEffect(() => {
+        // console.log('this runs every time when the state variables change')
+        console.log('this code only runs once on component mount')
+        fetchItems()
+    }, [])
+
+
+
 
     const onInputChange = async (ev) => {
         // console.log(ev)
@@ -18,8 +48,35 @@ function Todos(props) {
         // spread operator
         // lookup updating arry in a functional component in react
         if(inputText.trim().length>0){
-            setList([...list, {str: inputText, status: 'active'}]);
-            setInputText('')
+            // fetch POST call to create a t0do item
+
+
+            // POST /todos
+            let res = await fetch('http://localhost:3000/api/v1/todos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify({itemTxt: inputText})
+            });
+
+            let data = await res.json();
+            console.log(data);
+            setInputText('');
+            fetchItems();
+
+            // fetchUsers();
+            // setFirstName('')
+            // setLastName('')
+
+
+
+
+
+
+            // setList([...list, {str: inputText, status: 'active'}]);
+            // setInputText('')
         }
     };
 
@@ -70,11 +127,26 @@ function Todos(props) {
 
             <br/>
             <br/>
-            {list.length>0 && <div className={'bg-gray-100 p-2 rounded'}>
+            {currentTab=='active' && list.length>0 && <div className={'bg-gray-100 p-2 rounded'}>
                 <ul>
                     {list.map((item, idx) => {
-                        return <TodoItem item={item} key={idx}/>
+                        if(!item.completed){
+                            return <TodoItem item={item}
+                                             fetchItems={fetchItems}
+                                             key={idx}/>
+                        }
+                    })}
+                </ul>
+            </div>}
 
+            {currentTab=='completed' && list.length>0 && <div className={'bg-gray-100 p-2 rounded'}>
+                <ul>
+                    {list.map((item, idx) => {
+                        if(item.completed){
+                            return <TodoItem item={item}
+                                             fetchItems={fetchItems}
+                                             key={idx}/>
+                        }
                     })}
                 </ul>
             </div>}
@@ -91,10 +163,10 @@ function Todos(props) {
             {/*</ul>*/}
             <br/>
             <br/>
-            <button onClick={() => {
-                console.log('i was here')
-                setCurrentTab('all')
-            }} className={currentTab=='all' ? 'p-0 rounded pl-3 pr-3 cursor-pointer text-white text-lg bg-blue-500' : 'p-0 rounded pl-3 pr-3 cursor-pointer text-white text-lg bg-gray-500'}>All</button>&nbsp;&nbsp;
+            {/*<button onClick={() => {*/}
+            {/*    console.log('i was here')*/}
+            {/*    setCurrentTab('all')*/}
+            {/*}} className={currentTab=='all' ? 'p-0 rounded pl-3 pr-3 cursor-pointer text-white text-lg bg-blue-500' : 'p-0 rounded pl-3 pr-3 cursor-pointer text-white text-lg bg-gray-500'}>All</button>&nbsp;&nbsp;*/}
             <button onClick={() => {
                 console.log('i was here')
                 setCurrentTab('active')
